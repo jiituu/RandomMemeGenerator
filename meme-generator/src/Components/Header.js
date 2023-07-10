098e2bc07eae9bc6c147293a759a91b31759fd92
Jitu <jiituuh@gmail.com>
2023-07-10 12:16:15 +0300

    Header done(kinda)

---
+++ meme-generator/src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+// import Images from "./src/images";
+
+export default function Header(){
+    return(
+        <div className="Header">
+            <h2 className="Text">Meme Generator</h2>
+            <img src=""            className="Image"/>
+
+        </div>
+    )
+}
