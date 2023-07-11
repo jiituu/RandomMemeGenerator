5a89ec8fccc566c6e69d0853f83aa96e2e461e93
Jitu <jiituuh@gmail.com>
2023-07-11 15:55:14 +0300

    Done with the s**tiest css you could think of

--- meme-generator/src/Components/Header.js
+++ meme-generator/src/Components/Header.js
@@ -1,12 +1,14 @@
 import React from "react";
 
-// import Images from "./src/images";
+import Images from "../images/troll-face.png";
 
 export default function Header(){
     return(
         <div className="Header">
+            <img src={Images}className="Image"/>
+
             <h2 className="Text">Meme Generator</h2>
-            <img src=""            className="Image"/>
+            <img src={Images}className="Image"/>
 
         </div>
     )
